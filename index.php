1ac6e14e8372da4164075039ecc2054a2d281dc7
Daniel <dbento001@gmail.com>
2019-01-14 10:11:58 -0200

    Metodos de Listar

--- index.php
+++ index.php
@@ -1,10 +1,24 @@
 <?php
 
   require_once("config.php");
- 
-  $root = new Usuario();
-  $root->loadbyId(1);
 
-echo $root;
+  //Carrega um usuário.
+  //$root = new Usuario();
+  //$root->loadbyId(1);
+  //echo $root;
+
+  //Carrega uma lista de usuários
+  //$lista = Usuario::getList();
+  //echo json_encode($lista);
+
+//carrega uma lista de usuários buscando pelo login
+//$search = Usuario::search("da");
+//echo json_encode($search);
+
+//carrega usuário usando login e a senha 
+$usuario =  new Usuario();
+$usuario->login ("root", "!@#%");
+
+echo $usuario;
 
 ?>
